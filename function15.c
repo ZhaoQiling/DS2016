9f053bd82ed4ed882a6c3fa18f13931e803e1ccf
“yZhao70 <l>
2017-10-21 02:11:50 +0800

    FactorialSum of a nodelist

---
+++ function15.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node *PtrToNode;
+struct Node {
+    int Data; /* 存储结点数据 */
+    PtrToNode Next; /* 指向下一个结点的指针 */
+};
+typedef PtrToNode List; /* 定义单链表类型 */
+
+int FactorialSum( List L );
+
+int main()
+{
+    int N, i;
+    List L, p;
+
+    scanf("%d", &N);
+    L = NULL;
+    for ( i=0; i<N; i++ ) {
+        p = (List)malloc(sizeof(struct Node));
+        scanf("%d", &p->Data);
+        p->Next = L;  L = p;
+    }
+    printf("%d\n", FactorialSum(L));
+
+    return 0;
+}
+int FactorialSum( List L ){
+    int sum = 0;
+    while(L != NULL){
+        int val = 1;
+        int i = 2;
+        for(; i <= L->Data; i++)
+            val *= i;
+        sum += val;
+        L = L->Next;
+    }
+    return sum;
+}
