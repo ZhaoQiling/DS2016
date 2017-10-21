1be9458c0b093d7849cfeac04c798aeecf141d2e
ZhaoQiling <qilingzhao8@gmail.com>
2017-10-21 08:33:09 +0800

    get the union set of two nodelists

---
+++ code16.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct Node Node;
+struct Node{
+    int key;
+    Node* pNext;
+};
+Node* createNodelist(){
+    Node *pHead, *pTail;
+    pHead = pTail = NULL;
+    int val;
+    while(scanf("%d", &val) && val != -1){
+        Node* pTemp = (Node*) malloc(sizeof(Node));
+        pTemp->key = val; pTemp->pNext = NULL;
+        if(pHead == NULL)
+            pHead = pTail = pTemp;
+        else
+            pTail = pTail->pNext = pTemp;
+    }
+    return pHead;
+}
+int main(){
+    Node* pHead1 = createNodelist();
+    Node* pHead2 = createNodelist();
+    int flag = 0;
+    if(pHead1 == NULL || pHead2 == NULL){
+        printf("NULL");
+        return 0;
+    }
+    while(pHead1 != NULL || pHead2 != NULL){
+        if(pHead1 == NULL){
+            pHead2 = pHead2->pNext;
+            continue;
+        }
+        if(pHead2 == NULL){
+            pHead1 = pHead1->pNext;
+            continue;
+        }
+        if(pHead1->key == pHead2->key){
+            if(flag == 1) printf(" ");
+            printf("%d", pHead1->key);
+            flag = 1;
+            pHead1 = pHead1->pNext;
+            pHead2 = pHead2->pNext;
+            continue;
+        }
+        if(pHead1->key < pHead2->key){
+            pHead1 = pHead1->pNext;
+        }
+        else if(pHead1->key > pHead2->key){
+            pHead2 = pHead2->pNext;
+        }
+    }
+    if(flag == 0){
+        printf("NULL");
+        return 0;
+    }
+    return 0;
+}
